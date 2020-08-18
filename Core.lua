a3e14f2d670aaf3d749c865cac74546374cda1c8
VexTrex <66855203+Vex87@users.noreply.github.com>
2020-08-18 15:53:01 -0400

    Core - 2.0.0
    - Removed useless functions and added needed ones.

--- Core.lua
+++ Core.lua
@@ -1,281 +1,22 @@
--- ServerScriptService
--- 08/16/20
--- VexTrexYT
-
 local Core = {}
 
--- // Instances \\ --
-
-function Core.NewInstance(Class, Properties)
-	
-	if not Class then
-		error("Missing Argument 1: Class Name")
+function Core.Round(n, Places)
+	if not Places then
+		return math.floor(n + 0.5)
 	end
+	return math.floor((n * 10 ^ Places) + 0.5) / 10 ^ Places
+end
+
+function Core.Get(Parent, Class)
+	local Tbl = {}
 	
-	local Temp = Instance.new(Class)
-	
-	if Properties then
-		for PropertyName, Property in pairs(Properties) do
-			Temp[PropertyName] = Property
+	for _,v in pairs(Parent:GetChildren()) do
+		if v:IsA(Class) then
+			table.insert(Tbl, v)
 		end
 	end
 	
-	return Temp	
-	
-end
-
-function Core.CloneInstance(instance, Properties)
-	
-	if not instance then
-		error("Missing Argument 1: instance (Instance)")
-	end
-	
-	local Temp = instance:Clone()
-	
-	if Properties then
-		for PropertyName, Property in pairs(Properties) do
-			Temp[PropertyName] = Property
-		end
-	end
-	
-	return Temp	
-	
-end
-
-function Core.ChangeProperty(Instances, Properties)
-	
-	if not Instances then
-		error("Missing Argument 1: Array of Instances")
-	end
-	
-	if not Properties then
-		error("Missing Argument 2: Properties (Table)")
-	end	
-	
-	for _,v in pairs(Instances) do		
-		for PropertyName, PropertyValue in pairs(Properties) do
-			v[PropertyName] = PropertyValue	
-		end			
-	end
-	
-	return Instances
-end
-
--- // Paths \\ --
-
-function Core.Find(Obj, Target, Classifier, Val)
-	
-	if not Obj then
-		error("Missing Argument 1: Object (Instance)")
-	end
-	
-	if not Target then
-		error("Missing Argument 2: Target (String)")
-	end	
-	
-	if not Classifier then
-		error("Missing Argument 3: Classifier (String)")
-	end
-	
-	local TempTbl = {}	
-	do
-		if Target == "Children" then
-			TempTbl = Obj:GetChildren()					
-		elseif Target == "Siblings" then		
-			TempTbl = Obj.Parent:GetChildren()
-			table.remove(TempTbl, table.find(TempTbl, Obj))		
-		elseif Target == "Descendants" then		
-			TempTbl = Obj:GetDescendants()		
-		elseif Target == "Ancestors" then	
-			repeat
-				Obj = Obj.Parent
-				if Obj then
-					table.insert(TempTbl, Obj)
-				end
-			until not Obj.Parent
-		end
-	end
-	
-	for _,v in pairs(TempTbl) do
-		if Classifier == "Class" then
-				
-			if not Val then
-				error("Missing Argument 4: Class Name (String)")
-			end			
-				
-			if v:IsA(Val) then
-				return v
-			end
-				
-		elseif Classifier == "Property" then
-			
-			if not Val then
-				error("Missing Argument 4: Properties (Table: KEY=PROPERTYNAME, VALUE=PROPERTYVALUE)")
-			end			
-			
-			local Pass = true
-			for PropertyName,PropertyValue in pairs(Val) do	
-				local Success = pcall(function()	
-					local p = v[PropertyName]
-				end)	
-					
-				if Success and v[PropertyName] ~= PropertyValue then
-					Pass = false
-				elseif not Success then
-					Pass = false
-				end						
-			end	
-				
-			if Pass then
-				return v
-			end					
-								
-		elseif Classifier == "Prefix" then
-				
-			if not Val then
-				error("Missing Argument 4: Prefix (String)")
-			end					
-				
-			if string.sub(v.Name, 1, #Val) == Val then
-				return v
-			end	
-				
-		elseif Classifier == "Suffix" then
-				
-			if not Val then
-				error("Missing Argument 4: Suffix (String)")
-			end						
-				
-			if string.sub(v.Name, #v.Name - #Val + 1, #v.Name) == Val then
-				return v
-			end		
-		end	
-	end
-	
-end
-
-function Core.Get(Obj, Target, Classifier, Val)
-	
-	if not Obj then
-		error("Missing Argument 1: Object (Instance)")
-	end
-	
-	if not Target then
-		error("Missing Argument 2: Target (String)")
-	end	
-	
-	if not Classifier then
-		error("Missing Argument 3: Classifier (String)")
-	end
-	
-	local TempTbl = {}	
-	local FinalTbl = {}
-	do
-		if Target == "Children" then
-			TempTbl = Obj:GetChildren()					
-		elseif Target == "Siblings" then		
-			TempTbl = Obj.Parent:GetChildren()
-			table.remove(TempTbl, table.find(TempTbl, Obj))		
-		elseif Target == "Descendants" then		
-			TempTbl = Obj:GetDescendants()		
-		elseif Target == "Ancestors" then	
-			repeat
-				Obj = Obj.Parent
-				if Obj then
-					table.insert(TempTbl, Obj)
-				end
-			until not Obj.Parent
-		end
-	end
-	
-	for _,v in pairs(TempTbl) do
-		if Classifier == "Class" then
-				
-			if not Val then
-				error("Missing Argument 4: Class Name (String)")
-			end			
-				
-			if v:IsA(Val) then
-				table.insert(FinalTbl, v)
-			end
-				
-		elseif Classifier == "Property" then
-			
-			if not Val then
-				error("Missing Argument 4: Properties (Table: KEY=PROPERTYNAME, VALUE=PROPERTYVALUE)")
-			end			
-			
-			if Val then
-				local Pass
-				
-				for PropertyName,PropertyValue in pairs(Val) do					
-					local Success = pcall(function()	
-						local p = v[PropertyName]
-					end)	
-					
-					if Success and v[PropertyName] == PropertyValue then
-						Pass = true
-					else
-						Pass = false
-					end						
-				end	
-				
-				if Pass then
-					table.insert(FinalTbl, v)
-				end					
-				
-			end	
-				
-		elseif Classifier == "Prefix" then
-				
-			if not Val then
-				error("Missing Argument 4: Prefix (String)")
-			end					
-				
-			if string.sub(v.Name, 1, #Val) == Val then
-				table.insert(FinalTbl, v)
-			end	
-				
-		elseif Classifier == "Suffix" then
-				
-			if not Val then
-				error("Missing Argument 4: Suffix (String)")
-			end						
-				
-			if string.sub(v.Name, #v.Name - #Val + 1, #v.Name) == Val then
-				table.insert(FinalTbl, v)
-			end		
-		end	
-	end
-	
-	return FinalTbl
-end
-
-function Core.Wait(Obj, Classifier, Val, MaxWait)
-	
-	if not Obj then
-		error("Missing Argument 1: Object (Instance)")
-	end
-	
-	if not Classifier then
-		error("Missing Argument 2: Classifier (String)")
-	end	
-	
-	local Found
-	if not MaxWait then		
-		repeat	
-			Found = Core.Find(Obj, "Children", Classifier, Val)	
-			wait(1)
-		until Found	
-	else			
-		for x = MaxWait, 0, -1 do	
-			Found = Core.Find(Obj, "Children", Classifier, Val)	
-			if Found then break end				
-			wait(1)
-		end		
-	end
-	
-	return Found
+	return Tbl
 end
 
 function Core.WaitForPath(target, path, maxWait)
@@ -307,6 +48,51 @@ function Core.WaitForPath(target, path, maxWait)
 	end
 	return latest
 	
+end
+
+function Core.Mag(Start, End)
+	
+	if typeof(Start) ~= "Vector3" then
+		Start = Start.Position
+	end
+	
+	if typeof(End) ~= "Vector3" then
+		End = End.Position
+	end	
+	
+	return (Start-End).Magnitude
+end
+
+function Core.NewThread(func,...)
+	local a = coroutine.wrap(func)
+	a(...)
+end
+
+function Core.TableRemove(Table, Value, RemoveCount)		
+	local Count = 0
+	
+	if typeof(RemoveCount) == "number" then
+		for x = RemoveCount, 1, -1 do
+			if table.find(Table, Value) then
+				Count = Count + 1
+				table.remove(Table, table.find(Table, Value))	
+			end
+		end
+	elseif RemoveCount then
+		repeat
+			if table.find(Table, Value) then
+				Count = Count + 1	
+				table.remove(Table, table.find(Table, Value))	
+			end
+		until not table.find(Table, Value)
+	elseif not RemoveCount then
+		if table.find(Table, Value) then
+			Count = Count + 1
+			table.remove(Table, table.find(Table, Value))
+		end
+	end
+	
+	return Count
 end
 
 return Core
