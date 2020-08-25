db5a149c6b60347eacc698b1e49ae0b02c373ed0
VexTrex <66855203+Vex87@users.noreply.github.com>
2020-08-25 02:16:16 -0400

    Made it actually work

--- FindTextInScript.lua
+++ FindTextInScript.lua
@@ -1,25 +1,13 @@
-local Msg = {"print"}
-local Targets = {}
+local Msg = {"UDim2.new"}
+local Targets = workspace.Rojo:GetDescendants()
 
-local coreGui = game:GetService("CoreGui")
-
-local descendants = game:GetDescendants()
-local isA = game.IsA
-
-local function couldBeMalicious(descendant)
-    local success, isScript = pcall(isA, descendant, "LuaSourceContainer")
-
-    if ((success and isScript) and (not descendant:IsDescendantOf(coreGui))) then
-        local source = descendant.Source:lower()
-
-        if (source:match("print")) then
-            return true
-        end
-    end
-end
-
-for i, descendant in pairs(descendants) do
-    if couldBeMalicious(descendant) then
-        warn(descendant:GetFullName())
-    end
+for _,Script in pairs(Targets) do
+	if Script:IsA("LuaSourceContainer") then
+		local Source = Script.Source:lower()		
+		for __,Txt in pairs(Msg) do
+			if Source:match(Txt:lower()) then
+				warn(Script:GetFullName())
+			end
+		end		
+	end
 end
