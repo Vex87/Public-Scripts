53f38d62fb24c7416c916acd4c77bd23e0a81b8b
VexTrex <66855203+Vex87@users.noreply.github.com>
2020-08-26 00:22:03 -0400

    Added Core.CreateEvent

--- Core.lua
+++ Core.lua
@@ -106,4 +106,33 @@ function Core.TableRemove(Table, Value, RemoveCount)
 	return Count
 end
 
+function Core.CreateEvent()
+	local Signal, Arguments = {}, {}
+	local Bindable = Instance.new("BindableEvent")
+	
+	function Signal:Connect(Callback)
+		return Bindable.Event:Connect(function()
+			Callback(unpack(Arguments, 1, Arguments[0]))
+		end)
+	end
+	
+	function Signal:Fire(...)
+		Arguments = {[0] = select("#", ...); ...}
+		Bindable:Fire()
+		Arguments = nil
+	end	
+	
+	function Signal:Wait()
+		Bindable.Event:Wait()
+		return unpack(Arguments, 1, Arguments[0])
+	end
+	
+	function Signal:Disconnect()
+		Bindable:Destroy()
+		Bindable, Arguments, Signal = nil
+	end
+	
+	return Signal
+end
+
 return Core
